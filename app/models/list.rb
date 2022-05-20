5312dea4c27036fab3ea3272a112f9a6e2273524
Kenzo Douchet <kenzodouchet33@gmail.com>
2022-05-20 12:34:17 +0200

    masuperwatchlist

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,6 @@
 class List < ApplicationRecord
   validates :name, presence: true, uniqueness: true
-
+  has_one_attached :bg_list
   has_many :bookmarks, dependent: :destroy
   has_many :movies, through: :bookmarks
 end
